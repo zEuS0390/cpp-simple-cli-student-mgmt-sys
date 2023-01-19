1b5f23b7f3940da3d99b33217f41a236229da8ca
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-01-19 15:52:01 +0800

    Use std::array instead of raw array

--- class.cpp
+++ class.cpp
@@ -3,12 +3,16 @@
 #include "class.hpp"
 
 Class::Class (std::string class_name):
-	total_students(0),
-	class_name(class_name) {
+	total_students{0},
+	class_name{class_name} {
 }
 
 void Class::addStudent(Student *student) {
-	*(students + total_students) = student;
+	if (total_students == MAX_STUDENTS) {
+		std::cout << "\n\tThe array is already full.\n";
+		return;
+	}
+	students[total_students] = student;
 	total_students++;
 }
 
@@ -23,3 +27,41 @@ void Class::displayAllStudents() {
 	std::cout << "\n\t=========================================\n";
 }
 
+int Class::findStudent(int student_number) {
+	int left = 0;
+	int right = total_students - 1;
+	int middle;
+	while (left <= right) {
+		middle = left + (right - left) / 2;
+		if (students[middle]->getStudentNumber() == student_number) {
+			return middle;
+		}
+		if (students[middle]->getStudentNumber() < student_number) {
+			left = middle + 1; 
+		} else {
+			right = middle - 1;
+		}
+	}
+	return -1;
+}
+
+void Class::deleteStudent(int student_number) {
+	if (total_students == 0) {
+		std::cout << "\n\tThe class is empty.\n";
+		return;
+	}
+	int index = findStudent(student_number);
+	if (index != -1) {
+		delete students[index];
+		students[index] = nullptr;
+		while (index < total_students-1) {
+			students[index] = students[index+1];
+			index++;
+		}
+		students[index] = nullptr;
+		total_students--;
+	} else {
+		std::cout << "\n\tStudent number doesn't exist!\n";
+	}	
+	return;
+}
--- class.hpp
+++ class.hpp
@@ -1,18 +1,21 @@
 #ifndef CLASS_H
 #define CLASS_H
 #include <string>
+#include <array>
 #include "student.hpp"
 #define MAX_STUDENTS 40
 
 class Class {
 	private:
 		std::string class_name;
-		Student *students[MAX_STUDENTS];
+		std::array<Student*, MAX_STUDENTS> students;
 		int total_students;
 	public:
 		Class(std::string);
 		void addStudent(Student*);
 		void displayAllStudents();
+		int findStudent(int);
+		void deleteStudent(int);
 };
 
 #endif
