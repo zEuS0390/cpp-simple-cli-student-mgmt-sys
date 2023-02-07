fe19f9629c5d4c5b73d90368abc47c68ca605f30
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-02-07 16:17:38 +0800

    Remove unnecessary includes and add few comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 #include <string>
-#include <functional>
 #include <stdexcept>
-#include <array>
 #include <cstdlib> 
 #include "student.hpp"
 #include "class.hpp"
 #include "options.hpp"
+#include "constants.hpp"
 #ifdef _WIN32
 #define CLEAR_COMMAND "cls"
 #else
 #define CLEAR_COMMAND "clear"
 #endif
 #define OPTION [](Class &section, bool &isRunning)
-#include "constants.hpp"
 
 inline void clear() { std::system(CLEAR_COMMAND); }
 inline void wait() { std::cout << "\n\tPress any key to continue..."; std::cin.get(); }
 
 // Main function
-int main (void) {
+int main () {
 	bool isRunning = true;
 	std::string select;
 	Class section("CPE1FB3");
+
+	// Create option items
 	Options options = {
 		{"Exit", OPTION{ option::exit(&isRunning); }}, 
 		{"Add Student", OPTION{ option::addStudent(&section); }},
@@ -30,12 +30,18 @@ int main (void) {
 		{"Delete Student", OPTION{ option::deleteStudent(&section); }},
 		{"Display Students", OPTION{ option::displayStudents(&section); }}
 	};
+
 	clear();
+
 	while (isRunning) {
-		std::cout << "\n\t" + std::string(50, '%') + "\n";
+		// Display options
 		options.display();
+
+		// Get input from the user
 		std::cout << "\tSelect an option [0-" + std::to_string(options.size()-1) + "]: ";
 		std::getline(std::cin, select);
+		
+		// Convert input string to unsigned int
 		unsigned int index;
 		try {
 			index = std::stoi(select);
@@ -44,11 +50,14 @@ int main (void) {
 			wait(); clear();
 			continue;
 		}
+
+		// Execute the requested function
 		if (index < options.size()) {
 			options.execute(index, section, isRunning);
 		} else {
 			std::cout << "\n\tSelected option is out of range!\n";
 		}
+
 		wait(); clear();
 	}
 	return 0;
